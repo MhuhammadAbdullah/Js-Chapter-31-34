9996cf7c0e221e75d688a6fe398417966d2efc2e
Muhammad Abdullah <abaloch0033@gmail.com>
2024-10-12 10:08:01 +0500

    updated

--- app.js
+++ app.js
@@ -57,17 +57,74 @@
 
 //QUESTION 9
 
-// var startingDate = new Date(2015, 4, 18); // May 18, 2015
-// var now = new Date();
+// var ramadanStartDate = new Date("June 18, 2015");
+// var currentDate = new Date();
+// var timeDifference = currentDate - ramadanStartDate;
+// var daysPast = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
-// // Calculate days passed directly using getTime
-// var daysPassed = Math.floor((now.getTime() - startingDate.getTime()) / (1000 * 60 * 60 * 24));
-
-// console.log(daysPassed + "  days have past since 1st Ramadan, 2015");
-
+// alert("Number of days past since 1st Ramadan (June 18, 2015): " + daysPast);
 
 
-// var startingdate = new Date(5, 18, 2015);
-// var now = new Date();
-// var n = now.getDate();
-// console.log(n - startingdate + " days has been pased");
+//QUESTION 10
+
+// var beginningOf2015 = new Date(2015, 0, 1);
+// var referenceDate = new Date('Dec 5, 2015 22:50:16');
+// var timeDifference = referenceDate.getTime() - beginningOf2015.getTime();
+// var secondsElapsed = Math.floor(timeDifference / 1000);
+// alert("On reference date " + referenceDate + ", " + secondsElapsed + " seconds had passed since the beginning of 2015.");
+
+
+//QUESTION 11
+
+// var currentDate = new Date();
+
+// document.write("Current date: " + currentDate + "<br>");
+
+// currentDate.setHours(currentDate.getHours() + 1);
+
+// document.write("1 hour ago, it was: " + currentDate);
+
+
+//QUESTION 12
+
+// var currentDate = new Date();
+
+// document.write("Current date: " + currentDate + "<br>");
+
+// currentDate.setFullYear(currentDate.getFullYear() - 100);
+
+// document.write("100 years back, it was: " + currentDate);
+
+//QUESTION 13
+
+// var userAge = prompt("Enter your age:");
+// var currentYear = new Date().getFullYear();
+// var birthYear = currentYear - userAge;
+
+// document.write("Your age is " + userAge + "<br>");
+// document.write("Your birth year is " + birthYear);
+
+//QUESTION 14
+
+var customerName = "ABC Customer";
+var currentMonth = "February";
+var numberOfUnits = 410;
+var chargesPerUnit = 16;
+
+var netAmountPayable = numberOfUnits * chargesPerUnit;
+
+var latePaymentSurcharge = 350;
+
+var grossAmountPayable = netAmountPayable + latePaymentSurcharge;
+
+netAmountPayable = netAmountPayable.toFixed(2);
+grossAmountPayable = grossAmountPayable.toFixed(2);
+
+document.write("<h2>K-Electric Bill</h2>");
+document.write("Customer Name: <strong>" + customerName + "</strong><br>");
+document.write("Month: <strong>" + currentMonth + "</strong><br>");
+document.write("Number of units: <strong>" + numberOfUnits + "</strong><br>");
+document.write("Charges per unit: <strong>" + chargesPerUnit + "</strong><br><br>");
+document.write("Net Amount Payable (within Due Date): <strong>" + netAmountPayable + "</strong><br>");
+document.write("Late payment surcharge: <strong>" + latePaymentSurcharge + "</strong><br>");
+document.write("Gross Amount Payable (after Due Date): <strong>" + grossAmountPayable + "</strong>");
